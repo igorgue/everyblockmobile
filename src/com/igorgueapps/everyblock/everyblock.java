fe3112c1b7f3d15a64ee07714fda34cde7c20023
Igor Guerrero Fonseca <igfgt1@gmail.com>
2009-02-22 02:59:32 -0500

    added gitignore

---
+++ src/com/igorgueapps/everyblock/everyblock.java
@@ -0,0 +1,13 @@
+package com.igorgueapps.everyblock;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class everyblock extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
